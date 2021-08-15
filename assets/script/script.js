c5b22238c20ecf757703d203ecef92518da2b2ad
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-08-14 20:56:34 -0400

    before submit

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,53 +3,51 @@ $(document).ready(function () {
     console.log("document ready!");
 }
 //find  all button clickevents on the page. 
-var currentDayEl = document.getElementById("currentDay");
 var saveBtnEl = document.querySelector("col-md-1");
-var userInputEl = document.querySelector("col-md-10 user-input");
+var textAreaEl = document.querySelector("col-md-10 user-input")
 //every row in the timeblock has its own id, grab them each 
 //(.getItemById)
-var hour9El = document.getElementById("hour-9");
-var hour10El = document.getElementById("hour-10");
-var hour11El = document.getElementById("hour-11");
-var hour12El = document.getElementById("hour-12");
-var hour13El = document.getElementById("hour-13");
-var hour14El = document.getElementById("hour-14");
-var hour15El = document.getElementById("hour-15");
-var hour16El = document.getElementById("hour-16");
-var hour17El = document.getElementById("hour-17");
 
 //grab the saved event options
 //create variables to save user input and time
 var toDoList = {
-    "9AM" = [];
-    "10AM" = [];
-    "11AM" = [];
-    "12PM" = [];
-    "1PM" = [];
-    "2PM" = [];
-    "3PM" = [];
-    "4PM" = [];
-    "5PM" = [];
-}
-console.log(toDoList);
+    "9AM" = [],
+    "10AM" = [],
+    "11AM" = [],
+    "12PM" = [],
+    "1PM" = [],
+    "2PM" = [],
+    "3PM" = [],
+    "4PM" = [],
+    "5PM" = [],
 
-//save to localStorage
-function(saveEvent) {
-    localStorage.setItem("toDoList", JSON.stringify(toDoList));
-    console.log(saveEvent);
+    console.log(toDoList);
 }
-//addEventListener('click');
-saveBtnEl.addEventListener('click', saveEvent);
-        
-        //after saving to localStorage, alert the user.
 
-        //create some kind of timeOut value that removes the notification.
+
+function timeBlock() {
+    //save to localStorage
+    localStorage.setItem("content", toDoList);
+    //Retrieve localStorage and render items to the correct rows. 
+    document.getElementById("9AM").innerHTML = localStorage.getItem("content");
+}  
+    //addEventListener('click');
+    saveBtnEl.addEventListener("click", timeBlock);
+
+    //after saving to localStorage, alert the user.
+    //create some kind of timeOut value that removes the notification.
+
+//need a for loop to loop through the timeBlocks
 
 //create time updater function ()
     //get the current time (moment.js)
+    var currentTimeEl = document.getElementById("currentDay");
+    currentTimeEl = text.moment().format('MMMM Do YYYYY, h:mm:ss a');
 
     //create our loop to go over all the time blocks (for-loop)
-    
+    for (var i = 0; i < toDoList.length; i++) {
+        console.log()
+    }
 
      //check to see if have passed out time
 
@@ -60,8 +58,16 @@ saveBtnEl.addEventListener('click', saveEvent);
      // (one if statement)
      //(create all the time elements)
 
+function blockColor (){
+    if (currentTimeEl > 9){
+     $("9AM").addClass("past")
+    } else if (currentTimeEl < 9) {
+        $("9AM").addClass("future")
+    } else("present");
+    
+}
 
-//Retrieve localStorage and render items to the correct rows. 
+
 // Get one of the rows working and copy and paste 
 
 
